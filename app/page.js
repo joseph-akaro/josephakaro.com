98cb4571f7aec6440a56aff13bfff9279b351c88
Joseph Akaro <josphmmakaro@gmail.com>
2025-07-02 17:09:07 +0200

    info Banner

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>
+    <div className='flex flex-col items-center bg-emerald-950 w-[50%] h-[60%] p-4 m-auto mt-20 rounded-xl'>
       <h1>Thanks for Checking by, am currently Developing this site.</h1>
       <p>Contact Me:</p>
       <p>info@stackymedia.com</p>

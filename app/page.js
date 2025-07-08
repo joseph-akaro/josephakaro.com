04dd8c4bb8ead526cb0ef9a4fbf97bb3a979ac13
Joseph Akaro <josphmmakaro@gmail.com>
2025-07-08 18:53:05 +0300

    Fixed callstack

--- app/page.js
+++ app/page.js
@@ -1,37 +1,11 @@
 'use client'
 
-import React, { useState } from 'react'
-import { Meteors } from '@/components/ui/meteors';
-
 const Home = () => {
-  const [ dev, setDev] = useState(false);
-  
-  const Development = () => {
-      setDev(false)
-  }
-
   return (
-    <div>
-      {Development ? (<Metero/>) : (<homePage/>)}
-    </div>
-  )
-}
-
-export default Home
-
-
-// This Loads Home Page component
-const homePage = () => {
-  return(
     <div>
       <h1>Welcome to Home!</h1>
     </div>
   )
 }
 
-// This Loads Coming-Soon Page Component
-const Metero = () => {
-  return(
-    <Metero/>
-  )
-}
+export default Home
